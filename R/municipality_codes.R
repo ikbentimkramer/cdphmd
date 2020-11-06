6eca8b7b0f0213c0339581f5a328231926b2a386
Augustin4762 <procopi.augustin@gmail.com>
2020-11-06 16:34:43 +0100

    Updated clean function
    Read function and clean function are now in separate files.
    Added new function that reads gemeente identification info.
    The updated clean function outputs a dataframe with gemeentenaam, provincienaam, year and housing stock.

--- R/municipality_codes.R
+++ R/municipality_codes.R
@@ -3,13 +3,15 @@
 #' Reads the identification data for municipalities
 #' 
 #' @return a dataframe with every municipality code,name and province
+#' @import readxl
+#' @importFrom utils download.file
 #' @export
 #'
 #' @examples 
-#' df = mun_code_to_name()
+#' df = read_municipality()
 read_municipality <- function(){
   url = "https://www.cbs.nl/-/media/_excel/2020/03/gemeenten-alfabetisch-2020.xlsx"
   temp.file <- paste(tempfile(),".xlsx")
-  download.file(url, temp.file, mode = "wb")
+  utils::download.file(url, temp.file, mode = "wb")
   tmp <- readxl::read_excel(temp.file)
 }
