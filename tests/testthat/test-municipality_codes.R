039e0ba4a9909bbe7985e1db06ed8a11eb16e384
ikbentimkramer <53525709+ikbentimkramer@users.noreply.github.com>
2020-11-09 14:00:44 +0100

    Modify/fix up tests

--- tests/testthat/test-municipality_codes.R
+++ tests/testthat/test-municipality_codes.R
@@ -1,4 +1,8 @@
 test_that("correctly reads municipality codes and respective names", {
+  temp <- mockery::mock("municipality_codes_test")
+  dontdownload <- mockery::mock()
+  mockery::stub(read_municipality, "utils::download.file", dontdownload)
+  mockery::stub(read_municipality, "tempfile", temp, depth = 2)
   data <- read_municipality()
   expect_identical(colnames(data), c("Gemeentecode", "GemeentecodeGM",
                                      "Gemeentenaam", "Provinciecode", "ProvinciecodePV",
