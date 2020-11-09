91ae2aa8b12ae65901d5198445eeaff9a82f4894
ikbentimkramer <53525709+ikbentimkramer@users.noreply.github.com>
2020-11-09 14:03:53 +0100

    Change undescriptive variable name to descriptive one

--- R/clean_housing_stock_data.R
+++ R/clean_housing_stock_data.R
@@ -13,8 +13,8 @@
 #' @noRd
 
 clean_housing_stock_data <- function(df,codes_df) {
-  y <- filter(codes_df, Provincienaam == "Groningen" | Provincienaam == "Drenthe")
-  df <- dplyr::left_join(y, df, by = c("GemeentecodeGM" = "Regions"))
+  codes_df <- filter(codes_df, Provincienaam == "Groningen" | Provincienaam == "Drenthe")
+  df <- dplyr::left_join(codes_df, df, by = c("GemeentecodeGM" = "Regions"))
   colnames(df)[which(colnames(df) == "Periods")] <- "year"
   colnames(df)[which(colnames(df) == "InitialStock_1")] <- "housing stock"
   colnames(df)[which(colnames(df) == "Gemeentenaam")] <- "municipality"
