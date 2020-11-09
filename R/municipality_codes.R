caa80f40c80c20b503a202f73b9101e441811ab6
ikbentimkramer <53525709+ikbentimkramer@users.noreply.github.com>
2020-11-09 14:02:09 +0100

    remove space in temp file name path

--- R/municipality_codes.R
+++ R/municipality_codes.R
@@ -1,17 +1,17 @@
 #' Municipality codes and names
 #'
 #' Reads the identification data for municipalities
-#' 
+#'
 #' @return a dataframe with every municipality code,name and province
 #' @import readxl
 #' @importFrom utils download.file
 #' @export
 #'
-#' @examples 
+#' @examples
 #' df = read_municipality()
 read_municipality <- function(){
   url = "https://www.cbs.nl/-/media/_excel/2020/03/gemeenten-alfabetisch-2020.xlsx"
-  temp.file <- paste(tempfile(),".xlsx")
+  temp.file <- paste0(tempfile(),".xlsx")
   utils::download.file(url, temp.file, mode = "wb")
   tmp <- readxl::read_excel(temp.file)
 }
