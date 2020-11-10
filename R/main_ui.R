af2b412021d8b32f219208e8da31f44e9761c557
Augustin4762 <procopi.augustin@gmail.com>
2020-11-10 11:45:26 +0100

    Updated main_ui.R
    connected the sidebar item to the body item

--- R/main_ui.R
+++ R/main_ui.R
@@ -1,7 +1,6 @@
 #' Main UI
 #'
 #' This function returns the main UI for this dashboard.
-#' @param housing_data housing stock data
 #'
 #' @examples
 #' \dontrun{
@@ -9,33 +8,49 @@
 #' }
 #' @import shiny
 #' @import shinydashboard
-#' @noRd
 main_ui  <- function(housing_data) {
   constants <- list(
     title = "CDPHMD")
-  shinydashboard::dashboardPage(
-    shinydashboard::dashboardHeader(title = constants[["title"]]),
-    shinydashboard::dashboardSidebar(),
-    shinydashboard::dashboardBody(
-      tags$head(
-        tags$link(
-          href = "https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap",
-          rel = "stylesheet",
-          type = "text/css"),
-        tags$link(
-          href = "www/css/cdphmd.css",
-          rel = "stylesheet",
-          type = "text/css")
-      ),
-      shiny::fluidRow(
-        shinydashboard::box(
-          dropdown_box_graph_ui(
-            "housing_stock",
-            line_graph_ui,
-            "Municipality",
-            unique(housing_data[,"municipality"])),
-          title = "Housing stock")
-      )
+  header <- shinydashboard::dashboardHeader(title = constants[["title"]])
+  
+  sidebar <- shinydashboard::dashboardSidebar(
+    sidebarMenu(
+      menuItem("General Information", tabName = "Intro", icon = icon("info")),
+      menuItem("Indicators", icon = icon("chart-line"), tabName = "indicators",
+               menuSubItem("Housing stock", tabName = "housingstock", icon = icon("building")),
+               menuSubItem("House Price", tabName = "houseprice", icon = icon("dollar-sign")),
+               menuSubItem("Indicator 3", tabName = "Indicator3")),
+      menuItem("Another item", tabName = "another")
     )
   )
+  
+  body <- shinydashboard::dashboardBody(
+    tags$head(
+      tags$link(
+        href = "https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap",
+        rel = "stylesheet",
+        type = "text/css"),
+      tags$link(
+        href = "www/css/cdphmd.css",
+        rel = "stylesheet",
+        type = "text/css")
+    ),
+    tabItems(
+      tabItem(tabName = "Intro", "General overview of the dashboard"),
+      tabItem("housingstock",
+              shiny::fluidRow(
+                shinydashboard::box(
+                  dropdown_box_graph_ui(
+                    "housing_stock",
+                    line_graph_ui,
+                    "Municipality",
+                    unique(housing_data[,"municipality"])),
+                  title = "Housing stock")
+              )),
+      tabItem("houseprice", "info about the 2nd indicator we will choose"),
+      tabItem("Indicator3", "info about the 3rd indicator we will choose")
+    )
+  )
+  shinydashboard::dashboardPage(header, sidebar, body)
 }
+
