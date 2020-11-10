78eae646442413d767788e8699d8c478d7b1dee8
ikbentimkramer <53525709+ikbentimkramer@users.noreply.github.com>
2020-11-10 12:02:17 +0100

    Fixes #37 - dplyr deprecation warning

--- tests/testthat/test-mod-line_graph.R
+++ tests/testthat/test-mod-line_graph.R
@@ -1,12 +1,26 @@
+input <- data.frame(foo = c(1, 2, 3, 4), bar = c(5, 5, 1, 3))
 test_that("line_graph_server does not throw errors", {
-  input <- data.frame(foo = c(1, 2, 3, 4), bar = c(5, 5, 1, 3))
-  shiny::testServer(
-    line_graph_server,
-    args = list(x = "foo", y = "bar", df = input),
-    {
-      expect_error(output$linegraph, NA)
-    })
+  rlang::with_options(lifecycle_verbosity = "quiet", {
+    shiny::testServer(
+      line_graph_server,
+      args = list(x = "foo", y = "bar", df = input),
+      {
+        expect_error(output$linegraph, NA)
+      })
+  })
 })
+
+test_that("line_graph_server throws a dplyr deprication warning", {
+  rlang::with_options(lifecycle_verbosity = "warning", {
+    shiny::testServer(
+      line_graph_server,
+      args = list(x = "foo", y = "bar", df = input),
+      {
+        expect_warning(output$linegraph, "dplyr")
+      })
+  })
+})
+
 test_that("column name strings get converted to formulas correctly",{
   expect_equal(string2formula("foo"), ~foo)
   expect_equal(string2formula(" foo"), ~` foo`)
