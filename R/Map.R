677f7d584ba5d9a5c9f9cdcce73b96851a3eca17
ikbentimkramer <53525709+ikbentimkramer@users.noreply.github.com>
2020-11-30 12:21:55 +0100

    Connect data to mapdata (WIP)

--- R/Map.R
+++ R/Map.R
@@ -1,22 +1,12 @@
-df = read.csv("gemeenten.csv")
-mapdata <- sf::st_as_sf(df, wkt = "geom")
-mapdata2 <- sf::st_cast(mapdata, "MULTIPOLYGON")
+test_map_f <- function() {
+  test <- sf::st_read("https://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/wfs?service=wfs&version=2.0.0&request=getFeature&typenames=cbsgebiedsindelingen:cbs_gemeente_2019_gegeneraliseerd&outputFormat=json")
 
-x = get_data("housing_data")
-#names(x)[1] <- "gemeentenaam"
+  df <- get_data("housing_data")
 
-df_map <- dplyr::right_join(x, mapdata2, by = by = c("Gemeentenaam", "gemeentenaam"))
-df_map <- sf::st_as_sf(df_map, wkt = "geom")
-df_map2 <- sf::st_cast(df_map, "MULTIPOLYGON")
+  test <- as_tibble(test)
+  test <- left_join(df, test, by = c("municip_code" = "statcode")) %>%
+    filter(year == 2019)
+  test <- st_sf(as.data.frame(test, stringsAsFactors = FALSE))
 
-
-fig <- plotly::plot_ly(data,
-               split = ~gemeentenaam,
-               showlegend = FALSE
-               )
-fig
-
-data = rjson::fromJSON(file = "https://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/wfs?service=wfs&version=2.0.0&request=getFeature&typenames=cbsgebiedsindelingen:cbs_gemeente_2019_gegeneraliseerd&outputFormat=json")
-#data <- as.data.frame(data)
-data1 <- sf::st_as_sf(data, wkt = "geom")
-data2 <- sf::st_cast(data1, "MULTIPOLYGON")
+  plotly::plot_ly(test, split = ~municip_code, color = ~`housing stock`)
+}
