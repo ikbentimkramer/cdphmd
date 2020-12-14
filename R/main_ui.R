9f139d8878c0b194c4792bef933ec10da6febaf2
ikbentimkramer <53525709+ikbentimkramer@users.noreply.github.com>
2020-12-14 14:31:57 +0100

    Move map to correct tab in UI

--- R/main_ui.R
+++ R/main_ui.R
@@ -44,20 +44,21 @@ main_ui  <- function(housing_data, housing_price) {
       tabItem(tabName = "Intro", "General information"),
       tabItem("Acc", "Accountability info"),
       tabItem("housingstock",
-              shiny::fluidRow(
-                shinydashboard::box(
-                  dropdown_box_graph_ui(
-                    "housing_stock",
-                    line_graph_ui,
-                    "Municipality",
-                    unique(housing_data[,"municipality"])),
-                  title = "Housing stock")
-              )),
-      tabItem("housingprice",
         shiny::fluidRow(
           shinydashboard::box(
             stock_map_ui(
               "housing_price_map")),
+
+          shinydashboard::box(
+            dropdown_box_graph_ui(
+              "housing_stock",
+              line_graph_ui,
+              "Municipality",
+              unique(housing_data[,"municipality"])),
+            title = "Housing stock")
+        )),
+      tabItem("housingprice",
+        shiny::fluidRow(
           shinydashboard::box(
             dropdown_box_graph_ui(
               "housing_price",
