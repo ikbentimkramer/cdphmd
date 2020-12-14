0adba58d4def0c11946725d6a0881f15d4992ce6
ikbentimkramer <53525709+ikbentimkramer@users.noreply.github.com>
2020-12-14 21:00:39 +0100

    Add subjective data

--- R/run.R
+++ R/run.R
@@ -13,5 +13,6 @@ run  <- function() {
   housing_data <- get_data("housing_data")
   housing_price <- get_data("housing_price")
   mapdata <- get_data("municip_map")
+  woon <- get_data("woon")
   shiny::shinyApp(main_ui(housing_data, housing_price), main_server(housing_data, housing_price, mapdata))
 }
