6080e1de42c9eb8f8a7e9ed2fa62d60b70a837bb
Augustin4762 <procopi.augustin@gmail.com>
2020-12-17 12:37:56 +0100

    fixed devtools error and the test

--- tests/testthat/test-clean_migration_data.R
+++ tests/testthat/test-clean_migration_data.R
@@ -1,14 +1,14 @@
 ## Set up data
 df <- data.frame(
-  Periods = c("2015JJ00", "2019JJ00", "2015JJ00", "2019JJ00", "2015JJ00", "2019JJ00"),
-  Regions = c("GM1699", "GM1699", "GM0093", "GM0093", "GM9875", "GM9875"),
-  PopulationOn1January_1 = c(31137, 31290, 4827, 4890, 3333, 2222),
+  Periods = rep(c("2015JJ00","2019JJ00"), 9),
+  Regions = rep(c("GM1699", "GM1699", "GM0093", "GM0093", "GM9875", "GM9875"), 3),
+  PopulationOn1January_1 = rep(c(31137,31290,4827,4890,3333,2222),3),
   stringsAsFactors = FALSE
 )
-codes_df <- codes_df <- data.frame(
-  GemeentecodeGM = c(rep("GM1699",2), rep("GM0093",2), rep("GM9875",2)),
-  Provincienaam = c(rep("Drenthe",2), rep("Friesland",2), rep("Groningen",2)),
-  Gemeentenaam = c(rep("Noordenveld",2), rep("Terschelling", 2), rep("Rommeldam",2)),
+codes_df <- data.frame(
+  GemeentecodeGM = c("GM1699", "GM0093", "GM9875"),
+  Provincienaam = c("Drenthe", "Friesland","Groningen"),
+  Gemeentenaam = c("Noordenveld", "Terschelling", "Rommeldam"),
   stringsAsFactors = FALSE)
 
 ## Set up result
