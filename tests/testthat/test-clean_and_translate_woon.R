c33e2485dc9bc0ba1fc3316f0405e03f719a229a
ikbentimkramer <53525709+ikbentimkramer@users.noreply.github.com>
2021-01-05 11:04:43 +0100

    Fix tests

--- tests/testthat/test-clean_and_translate_woon.R
+++ tests/testthat/test-clean_and_translate_woon.R
@@ -1,11 +1,19 @@
+mock_data <- tibble::tibble(
+  ldl = 1,
+  corop = 2,
+  leeftijd = 3,
+  tevrstr = 4,
+  verhwens = 5,
+  verleegst = 3,
+  coropchar = "Test")
+mockery::stub(clean_and_translate_woon, "get_data", mock_data)
+
 test_that("No errors are thrown", {
-  mock_data <- tibble::tibble(
-    ldl = 1,
-    corop = 2,
-    leeftijd = 3,
-    tevrstr = 4,
-    verhwens = 5,
-    verleegst = 3)
-  mockery::stub(clean_and_translate_woon, "get_data", mock_data)
-  expect_error(clean_and_translate_woon, NA)
+  expect_error(clean_and_translate_woon(), NA)
 })
+
+test_that("returns coropchar column", {
+  res <- clean_and_translate_woon()
+  expect_true("coropchar" %in% names(res))
+})
+
