6aff15ccb009703ca9ae92c8f15aa9050104861d
ikbentimkramer <53525709+ikbentimkramer@users.noreply.github.com>
2021-01-05 16:36:38 +0100

    Make woon survey data path an argument of run()

--- R/run.R
+++ R/run.R
@@ -1,19 +1,19 @@
 #' Start Dashboard
 #'
 #' This function starts the dashboard.
-#'
+#' @param woondata The path to the woon data file
 #' @examples
 #' \dontrun{
 #' run()
 #' }
 #' @import shiny
 #' @export
-run  <- function() {
+run  <- function(woondata = file.path(getwd(), "cache", "WoON2018_e_1.0.sav")) {
   shiny::addResourcePath('www', system.file('www', package = 'cdphmd'))
   housing_data <- get_data("housing_data")
   housing_price <- get_data("housing_price")
   mapdata <- get_data("municip_map")
-  woon <- get_data("woon")
+  woon <- get_data("woon", woonpath = woondata)
   shiny::shinyApp(
     main_ui(
       housing_data,
