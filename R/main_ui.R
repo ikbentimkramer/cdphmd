a61dbb17b275f0dc8a383f19e4bac4c2c8ecc2be
ikbentimkramer <53525709+ikbentimkramer@users.noreply.github.com>
2021-01-11 14:33:05 +0100

    implement logos

--- R/main_ui.R
+++ R/main_ui.R
@@ -89,7 +89,29 @@ main_ui  <- function(housing_data, housing_price, woon) {
 
       shinydashboard::tabItem(
         tabName = "Acc",
-        shiny::includeMarkdown(this_pkg("www/md/accountability.md"))),
+        shiny::fluidRow(
+          shinydashboard::box(
+            width = 12,
+            shiny::includeMarkdown(this_pkg("www/md/accountability.md")))),
+        shiny::fluidRow(
+          shinydashboard::box(
+            width = 3,
+            shiny::img(
+              alt = "Logo Rijksuniversiteit Groningen",
+              src = "./www/images/rugr_logoenv_rood_rgb.png",
+              style = "width: 100%")),
+          shinydashboard::box(
+            width = 6,
+            shiny::img(
+              alt = "Logo minor Data Wise",
+              src = "./www/images/datawise-logo.png",
+              style = "width: 100%")),
+          shinydashboard::box(
+            width = 3,
+            shiny::img(
+              alt = "Logo CBS",
+              src = "./www/images/cbs-ld-logo.png",
+              style = "width: 100%")))),
 
       shinydashboard::tabItem(
         tabName = "download",
