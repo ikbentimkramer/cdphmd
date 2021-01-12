0e034f85ce076247fe1f527e69a38169756cb531
Augustin4762 <procopi.augustin@gmail.com>
2021-01-12 12:02:33 +0100

    Create test-mod-barplot_migration_server.R
    dummy test, just a copy of barplot_server test

---
+++ tests/testthat/test-mod-barplot_migration_server.R
@@ -0,0 +1,21 @@
+input <- data.frame(xax = c(1, 2, 3, 4), yax = c(5, 5, 1, 3))
+test_that("barplot_server does not throw errors", {
+  rlang::with_options(lifecycle_verbosity = "quiet", {
+    shiny::testServer(
+      barplot_migration_server,
+      args = list(x = "foo", y = "bar", df = input),
+      {
+        expect_error(output$barplot, NA)
+      })
+  })
+})
+test_that("barplot_server accepts reactives", {
+  rlang::with_options(lifecycle_verbosity = "quiet", {
+    shiny::testServer(
+      barplot_server,
+      args = list(x = "xax", y = "yax", df = shiny::reactive({input})),
+      {
+        expect_error(output$barplot, NA)
+      })
+  })
+})
